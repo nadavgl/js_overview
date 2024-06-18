5d9015396bd85adbe767af95a3f35e9eabe98ddb
nadavgl <ngliksht@gmail.com>
2024-06-18 10:23:10 -0400

    finished console basics

---
+++ JS/script.js
@@ -0,0 +1,10 @@
+        // Javascript works wiht or has a set of different values
+        // A word or multiple words wrapped in quotes is called a String value type
+        // A number is called a Number value type
+        var firstName = 'Nadav';
+    
+        console.log(typeof 3);
+
+        console.log(5 * 5);
+
+        console.log(firstName)
