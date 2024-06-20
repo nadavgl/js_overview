11799c9ac03d0c9477868be7d6bb3190219013b3
nadavgl <ngliksht@gmail.com>
2024-06-20 12:57:14 -0400

    finished basics of loops

--- JS/script.js
+++ JS/script.js
@@ -2,35 +2,27 @@
 //0, null, undefined,'',NaN
 
 
-var firstName = 'Nadav'; //string
-var lastName = 'Glikshtern'
-var age = 18; // Number
+
 var names = ['John', 'Ann', 'Bob', 'Frank', 'Jane', 'Jimmy', 'Billy', 'Julie', 'Will']//Array
 var users = ['Bob', 99, 'Ann', 35,'Billy', 45]
-var oldEnough = age >= 21; //true
 
-// If Conditionals - Trigger if the VALUE you pass into the parantheses is a truthy value
 
-//Conditionals statments can only take ONE value
+//Print a count from 1 to 10
 
-if(firstName && lastName){
-    console.log('Received your full name!');
-} else if (!firstName) {
-    console.log('You must provide your first name!')
-} 
-else if (!lastName)  {
-    console.log('You must provide you last name')
-}
-else  {
-    console.log('You must provide you full name')
+// console.log(names[names.length - 1])
+
+// for (var count = 1; count <= 10; count++) {
+//     console.log(count);
+
+// }
+
+for (var index = 0; index < names.length; index++){
+    console.log(users[index])
 }
 
-if (oldEnough) {
-    console.log ('Congrats come in!')
-// Web API - Browser Toolset that we can access from within our JS
-alert ('Welcome ' + firstName)
-}
+var count = 1;
 
-else { console.log ('Get outta here kid!');
-alert ('No Chance...')
+while (count <= 10){
+    console.log(count)
+    count ++;
 }
