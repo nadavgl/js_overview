593b761b5371821887ba8201d29ddb121fb87b0f
nadavgl <ngliksht@gmail.com>
2024-06-21 13:42:07 -0400

    finished objects basics

--- JS/script.js
+++ JS/script.js
@@ -1,102 +1,47 @@
-// // function myFirstFunc(){
-// //     console.log('function has been called');
+// const str = 'some-string';
+// const num = 10;
+// const bool = true;
+// const arr = ['John', 'Bob', 'Steve'];
+
+// const data = {
+//     name: 'Nadav',
+//     age: 33
+
+// };
+
+// data.name = 'Bob'
+// data.age ++
 
 
-// // myFirstFunc();
+// console.log(data.name);
 
-// // function add(num1, num2){
-// //     console.log(num1 + num2)
+//Dot notation can ONLY be used on Obejcts
 
-// // }
-
-// // add(5, 10);
-
-// // add(30, 58)
-
-// function printValues (num, name, boolean, another){
-//     console.log(another);
-// }
-
-// printValues (10, 'Allen', false);
-
-// function printNames(names){
-//     for (const name of names) {
-//         console.log(name);
-//     }
-
-
-// }
-
-// printNames (['Bob','Jimmy','Julie'])
-
-// printNames ('Jake', 'Frank', 'Katie')
-
-
-
-/*
-Create a fucntion that takes 3 arguments
--The first and second argument will be a number
--The third argument will be a mathematical operator (+,-,*,/)
-
-Example: If the operator is a '+' then you will add the two numbers together and console log the sum
-
-*/
-
-// CAll your function 4 times with different numbers and operators to confirm that it is working correctly
-
-// Function calculate (num1, num2, operator) {
-
-//     if (operator === '+'){
-//         console.log(num1 + num2);
-//     }
-
-//     else if (operator === '-') {
-//         console.log(num1 - num2);
-//     }
-
-//     else if (operator === '*' ){
-//         console.log(num1 * num2);
-//     }
-
-//     else if (operator === '/'){
-//         console.log(num1 / num2);
-//     }
-
-// }
-
-// calculate(10,20, '+');
-
-function calculate(num1, num2, operator){
-    switch(operator){
-        case '+':
-            return num1 + num2;
-        case '-':
-            return num1 - num2;
-        case "*":
-            return num1 * num2;
-            
-        case '/':
-            return num1 / num2;
-        default:
-            console.log('You must provide a valid operator')
-
+const data = {
+    name: 'JD',
+    age: 44,
+    info: {
+      location: 'Atl',
+      hobbies: [
+        {
+          name: 'Fishing',
+          frequency: 'once every two weeks'
+        },
+        {
+          name: 'Pickleball',
+          frequency: 'once a week'
+        },
+        
+        {
+            name: 'Another',
+            frequency: 3
+        }
+      ]
     }
+  };
 
-}
+  console.log(data.info.hobbies[1,2].frequency)
 
-const sum = calculate (10,20, '+')
-console.log(sum)
-
-// 
-
-function add(num1, num2){
-    return 'another'
-
-}
-
-const answer = add()
-
-console.log (answer)
 
 
 
